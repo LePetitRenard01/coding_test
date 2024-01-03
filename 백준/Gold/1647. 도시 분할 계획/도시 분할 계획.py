fca5c7c15d47e74dcf5275faefbf06ad1e24bce0
shinjw01 <65654552+shinjw01@users.noreply.github.com>
2024-01-03 23:08:57 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 4272 ms, Memory: 271884 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -0,0 +1,30 @@
+#MST kruskal - edge 중심
+from sys import stdin
+if __name__ == '__main__':
+  n, m = map(int,stdin.readline().split())
+  graph = []
+  for _ in range(m):
+    tmp = list(map(int,stdin.readline().split()))
+    graph.append(tmp)
+  graph.sort(key = lambda x : x[2])
+  parent = [i for i in range(n+1)]
+  def find(n):
+    if n != parent[n]:
+      parent[n] = find(parent[n])
+    return parent[n]
+  
+  maxWeight = 0
+  res = 0
+  for i in graph:
+    p1 = find(i[0])
+    p2 = find(i[1])
+    if p1 == p2 :
+      continue
+    if p1 > p2 :
+      parent[p1] = p2
+    else:
+      parent[p2] = p1
+    maxWeight = max(maxWeight, i[2])
+    res += i[2]
+  
+  print(res - maxWeight)
