90f7a7dc6748138494e461150f1db478f2298659
shinjw01 <65654552+shinjw01@users.noreply.github.com>
2024-02-18 22:06:22 +0900

    [Gold IV] Title: 최단경로, Time: 576 ms, Memory: 68380 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,30 +1,32 @@
 from sys import stdin
 import heapq
+def solution():
+  v, e = map(int, stdin.readline().split())
+  k = int(stdin.readline())
+  edge = [[] for _ in range(v+1)]
+  for _ in range(e):
+    tmp = list(map(int, stdin.readline().split()))
+    edge[tmp[0]].append((tmp[1], tmp[2]))
 
-INF = float('inf')
-v, e = map(int,stdin.readline().split())
-k = int(stdin.readline())
-graph = [[] for _ in range(v+1)]
-dist = [INF for _ in range(v+1)]
-dist[k] = 0
-for i in stdin.read().splitlines():
-  i = list(map(int,i.split()))
-  graph[i[0]].append((i[2],i[1]))
+  distance = [float('INF')] * (v+1)
+  distance[k] = 0
+  q = [(0, k)]
 
-def dijkstra(graph, start):
-  q = []
-  heapq.heappush(q,(0, start))
   while q:
-    current_d, u = heapq.heappop(q)
-    if current_d > dist[u] :
+    dist, current = heapq.heappop(q)
+    if dist > distance[current] :
       continue
-    for i in graph[u]:
-      distance = i[0] + current_d
-      if dist[i[1]] > distance:
-        dist[i[1]] = distance
-        heapq.heappush(q,(distance,i[1]))
+    for next, weight in edge[current] :
+      if distance[next] <= dist + weight:
+        continue
+      distance[next] = dist + weight
+      heapq.heappush(q, (distance[next], next))
+  
+  for i, d in enumerate(distance[1:]):
+    if d == float('INF'):
+      print('INF')
+    else:
+      print(d)
 
-dijkstra(graph,k)
-
-for i in dist[1:]:
-  print(i if i !=INF else 'INF')
+if __name__ == '__main__':
+  solution()
