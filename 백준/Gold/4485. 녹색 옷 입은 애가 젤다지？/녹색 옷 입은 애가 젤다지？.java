c08b3eddcae700c80d3b71071fb29b337d7c6ee2
shinjw01 <65654552+shinjw01@users.noreply.github.com>
2025-01-22 12:39:10 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 228 ms, Memory: 19984 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,66 +1,48 @@
 import java.util.*;
 import java.io.*;
-
-
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	static int[][] d = {{1,0},{-1,0},{0,1},{0,-1}};
-	
-	public static void main(String[] args) throws IOException {
-		int n = Integer.valueOf(br.readLine());
-		int idx = 1;
-		while(n != 0) {
-			solution(n, idx++);
-			n = Integer.valueOf(br.readLine());
-		}
-		
-		
-		br.close();
-		bw.close();
-	}
-	
-	static void solution(int num, int idx) throws IOException {
-		
-		int answer = 0;
-		int map[][] = new int[num][num];
-		for (int i = 0; i < num; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < num; j++) {
-				map[i][j] = Integer.valueOf(st.nextToken());
-			}
-		}
-		
-		answer = dijkstra(map);
-		bw.write("Problem "+idx+": "+ answer+"\n");
-	}
-	
-	static int dijkstra(int[][] map) {
-		int n = map.length;
-		int m = map[0].length;
-		int[][] cost = new int[n][m]; //{0,0}에서 해당 좌표까지의 최소 cost
-		for(int[] c : cost) {
-			Arrays.fill(c, Integer.MAX_VALUE);
-		}
-		cost[0][0] = map[0][0];
-		PriorityQueue<int[]> queue = new PriorityQueue<>((a,b) -> Integer.compare(a[2], b[2]));
-		queue.add(new int[] {0,0,cost[0][0]});
-		
-		while(!queue.isEmpty()) {
-			int[] cur = queue.poll();
-			int curCost = cur[2];
-			for (int i = 0; i < 4; i++) {
-				int nX = cur[0] + d[i][0];
-				int nY = cur[1] + d[i][1];
-				if(nX < 0 || nX >= n || nY < 0 || nY >= m)
-					continue;
-				if(cost[nX][nY] <= map[nX][nY] + curCost)
-					continue;
-				cost[nX][nY] = map[nX][nY] + curCost;
-				queue.add(new int[] {nX,nY,cost[nX][nY]});
-			}
-		}
-			
-		return cost[n-1][m-1];
-	}
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        int idx = 1;
+        while (true) {
+            int n = Integer.parseInt(br.readLine());
+            if (n == 0) break;
+            bw.write("Problem "+idx+++": "+test(n)+"\n");
+        }
+        bw.flush();
+    }
+
+    private static int test(int n) throws IOException {
+        int[][] map = new int[n][n];
+        int[][] visited = new int[n][n];
+        for (int[] v : visited) Arrays.fill(v, Integer.MAX_VALUE);
+        int[][] direction = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[2] - b[2]);
+        pq.offer(new int[]{0,0, map[0][0]});
+
+        while(!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            if(visited[cur[0]][cur[1]] < cur[2]) continue;
+            visited[cur[0]][cur[1]] = cur[2];
+            for(int[] d : direction) {
+                int nx = cur[0] + d[0];
+                int ny = cur[1] + d[1];
+                if(nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
+                if(visited[nx][ny] <= cur[2] + map[nx][ny]) continue; //이미 더 적은 횟수로 방문했다면 컨티뉴
+                visited[nx][ny] = cur[2] + map[nx][ny];
+                pq.offer(new int[]{nx, ny, visited[nx][ny]});
+            }
+        }
+
+        return visited[n-1][n-1];
+    }
 }
