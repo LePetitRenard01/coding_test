49959299895eac766e49a13a49568e23330b4073
LePetitRenard01 <shinjw0131@naver.com>
2026-01-07 09:20:13 +0900

    [Gold IV] Title: 네트워크 연결, Time: 524 ms, Memory: 51720 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -1,76 +1,66 @@
-import java.io.*;
 import java.util.*;
-
-class Edge implements Comparable<Edge>{
-    int dest;
-    int dist;
-    public Edge(int dest, int dist) {
-        this.dest = dest;
-        this.dist = dist;
-    }
-    public int compareTo(Edge o) {
-        return dist - o.dist;
-    }
-}
-
+import java.io.*;
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringTokenizer st;
-    static int n;
-    static int m;
-    public static void main(String[] args) throws IOException {
-        n = Integer.parseInt(br.readLine());
-        m = Integer.parseInt(br.readLine());
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	//kruskal edge 
+	static int parents[];
+	public static void main(String[] args) throws IOException {
 
-        bw.write(prim()+"");
-        bw.flush();
-        bw.close();
-        br.close();
-    }
+		int n = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
 
-    static int prim() throws IOException {
-        //선이 너무 많으니까 선 업데이트 방식으로
-        // HashMap<Integer, Integer> dest, dist?
-        //그럼 정렬 어떻게 하지? 그냥 Edge에 넣으면 되지.
-        ArrayList<HashMap<Integer, Integer>> graph = new ArrayList<>();
-        for (int i = 0; i < n+1; i++) {
-            graph.add(new HashMap<>());
-        }
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            if (graph.get(a).containsKey(b)) {
-                graph.get(a).put(b, Math.min(graph.get(a).get(b), c));
-            }
-            else graph.get(a).put(b, c);
+		parents = new int[n+1];
+		for (int i = 0; i < n; i++) {
+			parents[i] = i;
+		}
+		
+		ArrayList<int[]> edges = new ArrayList<>();
+		
+		StringTokenizer st;
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
 
-            if (graph.get(b).containsKey(a)) {
-                graph.get(b).put(a, Math.min(graph.get(b).get(a), c));
-            }
-            else graph.get(b).put(a,c);
-        }
+			edges.add(new int[] {a,b,c});
+		}
+		
+		Collections.sort(edges, (a,b) -> a[2] - b[2]);
+		
+		int res = 0;
+		int cnt = 0;
+		for (int[] e : edges) {
+			if(find(e[0]) != find(e[1])) {
+				res += e[2];
+				union(e[0], e[1]);
+				if (++cnt == n-1) {
+					break;
+				}
+			}
+		}
+		
+		bw.write(res+"");
+		
+		bw.flush();
+		bw.close();
+		br.close();
+		
+	}
+	
+	private static int find(int a) {
+		if(parents[a] == a) return a;
+		return parents[a] = find(parents[a]);
+	}
 
-        boolean visited[] = new boolean[n+1];
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        pq.add(new Edge(1, 0));
-
-        int result = 0;
-        int cnt = 0;
-        while(cnt < n){
-            Edge e = pq.poll();
-
-            if(visited[e.dest]) continue;
-            visited[e.dest] = true;
-            cnt++;
-            result += e.dist;
-            //큐에 엣지 넣기
-            for (int i : graph.get(e.dest).keySet()){
-                pq.add(new Edge(i, graph.get(e.dest).get(i)));
-            }
-        }
-        return result;
-    }
+	private static boolean union(int a, int b) {
+		int parentA = find(a);
+		int parentB = find(b);
+		
+		if(parentA > parentB) parents[parentA] = parentB;
+		else if (parentA < parentB) parents[parentB] = parentA;
+		else return false;
+		return true;
+	}
 }
